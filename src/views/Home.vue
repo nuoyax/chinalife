ac04dd3e5cdd95786dbf99511842130e35a76834
developer <tester@qq.com>
2019-06-15 19:16:54 +0800

    2019年6月15日 19:16:44

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,53 @@
 <template>
-  <el-container >
-    <el-aside width="12%"   style="height:100%;text-align: left;background: #545c64">
-<el-menu
-  collapse-transition
-  unique-opened
-style="height:100%;text-align: left;"
- router
-  class="el-menu-vertical-demo el-col-24"
-  background-color="#545c64"
-  text-color="#fff"
-  default-active="/HomeIndex"
-  active-text-color="#ffd04b">
+  <el-container  >
+    <el-container style="width:15%;position: absolute;top: 0px;bottom: 0" >
+    <el-aside width="100%" style="background: #545c64;height: 100%"  >
 
-  <el-menu-item index="/HomeIndex" style="font-size: 10px;">
-    <template slot="title">
-      <i class="el-icon-s-home"></i>
-      <span>首页</span>
-    </template>
-    </el-menu-item>
+        <el-menu
 
+          style=" text-align: left;height: 100%"
+          router
+          class="el-menu-vertical-demo el-col-24"
+          background-color="#545c64"
+          text-color="#fff"
+          default-active="/HomeIndex"
+          active-text-color="#ffd04b">
 
-  <el-submenu :index="item.id" v-for="item in sub_menu" :key="item.id">
-    <template slot="title">
-      <i class="el-icon-location"></i>
-      <span>{{item.text}}</span>
-    </template>
-    <el-menu-item-group>
-      <el-menu-item style="font-size: 12px;" @click="menuEvent(item.text)" :index="items.index" v-for="items in  item.children" :key="items.index"  >
-        <i class="el-icon-alarm-clock"></i>{{items.text}}
-
-      </el-menu-item>
-
-    </el-menu-item-group>
-
-  </el-submenu>
+          <el-menu-item index="/HomeIndex" style="font-size: 10px;">
+            <template slot="title">
+              <i class="el-icon-s-home"></i>
+              <span>首页</span>
+            </template>
+          </el-menu-item>
 
 
+          <el-submenu :index="item.id" v-for="item in sub_menu" :key="item.id">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{item.text}}</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item style="font-size: 12px;" @click="menuEvent(item.text)" :index="items.index" v-for="items in  item.children" :key="items.index"  >
+                <i class="el-icon-alarm-clock"></i>{{items.text}}
+
+              </el-menu-item>
+
+            </el-menu-item-group>
+
+          </el-submenu>
 
 
-</el-menu>
+
+
+        </el-menu>
+
+
 
 
     </el-aside>
+    </el-container>
 
-    <el-container   >
+    <el-container   style="width:85%;position: absolute;left: 15%;top: 0px;bottom: 0px" >
       <el-header style="background: #fff;color:#000;text-align: left; line-height: 60px;height: 60px;box-shadow: 1px 1px 4px rgba(0,0,0,0.2)">
       <div  class="el-col-2" style="height: inherit;line-height: 86px" > <img class="" style="width:100px;height: 35px;" src="../assets/logo/logo.png" /></div>
       <div class="el-col-1"></div>
@@ -159,4 +163,7 @@ style="height:100%;text-align: left;"
   >>>.el-submenu__title {
     height: 40px;
     line-height: 40px;}
+  .el-aside {
+
+  }
 </style>
